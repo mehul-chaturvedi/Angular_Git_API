acca9b08b341e9c5c52c82987a09151463e49191
Mehul Chaturvedi <mechat0141@gmail.com>
2019-05-31 11:10:24 +0530

    Final Commit

--- src/app/chart-dialog/chart-dialog.component.ts
+++ src/app/chart-dialog/chart-dialog.component.ts
@@ -10,23 +10,87 @@ import { GitServices } from '../services/git.service';
 export class ChartDialogComponent implements OnInit {
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private gitService: GitServices) { }
-
+  chartdata: any;
+  // Clables:any = [];
+  count = 10;
+  start = 1;
+  nextp = false;
+  prevp = false;
   public barChartOptions = {
     scaleShowVerticalLines: false,
     responsive: true
   };
-  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels = [];
   public barChartType = 'bar';
   public barChartLegend = true;
-  public barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Counts This Week'},
-  ];
+  public barChartData = [{ data: [], label: 'Counts This Week' }];
+
   ngOnInit() {
     console.log(this.data, 'data');
     this.gitService.getRepoCommitCount(this.data.owner, this.data.repo).subscribe(res => {
-      this.barChartLabels = res['all'].keys() + 1;
-      this.barChartData.data = res['all'];
+      this.chartdata = res;
+      for (var i = 1; i <= 10; i++) {
+        this.barChartData[0].data.push(res['all'][i - 1]);
+        this.barChartLabels.push('Week ' + i);
+        // this.Cdata.push(res['all'][i-1]);
+        // this.Clables.push('Week '+ i);
+      }
+      console.log(this.chartdata, 'new')
     });
+  }
+
+  nextPage() {
+    this.nextp = true;
+    this.start = this.start + 10;
+    this.count = this.count + 10;
+    this.barChartData[0].data = [];
+    this.barChartLabels = [];
+    if (this.count <= 40) {
+      for (var i = this.start; i <= this.count; i++) {
+        this.barChartData[0].data.push(this.chartdata.all[i - 1]);
+        this.barChartLabels.push('Week ' + i);
+        // this.Cdata.push(res['all'][i-1]);
+        // this.Clables.push('Week '+ i);
+      }
+      console.log(this.barChartData[0].data, 'data')
+    } else {
+      this.prevp = true;
+      this.count = 52;
+      for (var i = this.start; i <= this.count; i++) {
+        this.barChartData[0].data.push(this.chartdata.all[i - 1]);
+        this.barChartLabels.push('Week ' + i);
+        // this.Cdata.push(res['all'][i-1]);
+        // this.Clables.push('Week '+ i);
+      }
+      console.log(this.barChartData[0].data, 'data')
+      this.count = 50;
+      this.start = 41;
+    }
+
+  }
+
+  previousPage() {
+    this.start = this.start - 10;
+    this.count = this.count - 10;
+    this.barChartData[0].data = [];
+    this.barChartLabels = [];
+    if (this.start >= 1) {
+      this.prevp = false;
+      for (var i = this.start; i <= this.count; i++) {
+        this.barChartData[0].data.push(this.chartdata.all[i - 1]);
+        this.barChartLabels.push('Week ' + i);
+        // this.Cdata.push(res['all'][i-1]);
+        // this.Clables.push('Week '+ i);
+      }
+      console.log(this.barChartData[0].data, 'data')
+
+    } else {
+      this.nextp = false;
+      this.prevp = false;
+      this.count = 0;
+      this.start = -9;
+    }
+    console.log(this.barChartData[0].data, this.start, 'data')
   }
 
 }
